050cd515899bb73988eca1945760b17933a632f8
Mykhailo Kunynets <xfreed1@gmail.com>
2021-02-08 12:45:41 +0200

    Added carousel

--- TestProject/scripts/main.js
+++ TestProject/scripts/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+  $('.carousel-slick').slick({
+        slidesToShow: 3,
+         slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows : false
+  });
+});
 function topFunction() {
     $("html, body").animate({
         scrollTop: 0
